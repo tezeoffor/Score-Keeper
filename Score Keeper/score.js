af0f40e2ff4536cbd6f7b855bc524660f4776c9e
tezeoffor <33659750+tezeoffor@users.noreply.github.com>
2019-02-28 17:30:49 +0000

    Add files via upload

---
+++ Score Keeper/score.js
@@ -0,0 +1,60 @@
+var p1Button = document.querySelector("#p1");
+var p2Button = document.getElementById("p2");
+var resetButton = document.querySelector("#reset");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
+var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("p span");
+var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
+
+// player 1 button
+p1Button.addEventListener("click", function(){
+	if (!gameOver) {
+		p1Score++;
+		if (p1Score === winningScore){
+			p1Display.classList.add("winner");
+			gameOver = true;
+		}
+	p1Display.textContent = p1Score;
+}	
+});
+
+//player 2 button
+p2Button.addEventListener("click", function(){
+	if (!gameOver) {
+		p2Score++;
+		if (p2Score === winningScore) {
+			p2Display.classList.add("winner");
+			gameOver = true;
+		}
+	p2Display.textContent = p2Score;
+	}
+	
+});
+
+// input
+numInput.addEventListener("change", function(){
+	winningScoreDisplay.textContent = numInput.value;
+	winningScore = Number(numInput.value);
+	reset();
+})
+
+// reset button
+resetButton.addEventListener("click", function(){
+	reset();
+
+});
+
+// reset function
+function reset(){
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = p1Score;
+	p2Display.textContent = p2Score;
+	p1Display.classList.remove("winner");
+	p2Display.classList.remove("winner");
+	gameOver = false;
+}
